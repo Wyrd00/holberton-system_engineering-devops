c895f15e33c8fc45a7ea9eb07496c2d02a0b3e91
Wyrd00 <amy.tai0120@gmail.com>
2018-07-18 22:01:50 -0700

    initial commit of fabfile

---
+++ 0x0B-web_server/fabfile.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python2.7
+# pack:creates a tar gzipped archive of the current directory
+# deploy:upload archive to remote server in /tmp/
+#       :creates directory /tmp/holbertonwebapp
+#       :untars archive into /tmp/holbertonwebapp
+# clean: delete tar file on local machine
+
+
+from fabric.api import *
+
+
+env.user = "ubuntu"
+env.password = ""
+env.key_filename = "~/.ssh/holberton"
+
+
+def pack():
+        """
+        pack: creates a tar gzipped archive of the current directory
+              and place in the local directory
+        """
+        local("local: tar --exclude='*.tar.gz' -cvzf holbertonwebapp.tar.gz .")
+
+
+def deploy():
+        """
+        deploy: upload archive to remote server in /tmp/
+                creates directory /tmp/holbertonwebapp
+                untars archive into /tmp/holbertonwebapp
+        """
+        run("mkdir /tmp/holbertonwebapp")
+        put("holbertonwebapp.tar.gz", "/tmp/holbertonwebapp")
+        run("cd /tmp/ && tar -xf holbertonwebapp.tar.gz -C /holbertonwebapp")
+
+
+def clean():
+        """
+        clean: delete tar file on local machine
+        """
+        local("rm holbertonwebapp.tar.gz")
