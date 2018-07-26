91caa8ab3a8e1c975ab5a55d3331b9662bbf3774
Amy <amy.tai0120@gmail.com>
2018-07-26 21:09:19 +0000

    puppet-lint

--- 0x0D-configuration_management/0-create_a_file.pp
+++ 0x0D-configuration_management/0-create_a_file.pp
@@ -1,9 +1,9 @@
 # create file in /tmp/
-file {'holberton' :
-    ensure     => file,
-    path       => '/tmp/holberton',
-    mode       => '0744',
-    owner      => 'www-data',
-    group      => 'www-data',
-    content    => 'I love Puppet'
+file { 'holberton' :
+    ensure  => file,
+    path    => '/tmp/holberton',
+    mode    => '0744',
+    owner   => 'www-data',
+    group   => 'www-data',
+    content => 'I love Puppet'
 }
