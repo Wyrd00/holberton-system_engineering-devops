5408b433b4706897eb59f632fb2372efe164cb24
Amy <amy.tai0120@gmail.com>
2018-08-28 05:15:18 +0000

    refactor codes

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -2,24 +2,23 @@
 '''
     script using REST API that returns employee TODO list progress
 '''
+
+
 import requests
 from sys import argv
 
 
 if __name__ == "__main__":
-    userRes = requests.get('https://jsonplaceholder.typicode.com/users/{}'
-                           .format(argv[1]))
-    user = userRes.json().get('name', None)
-
+    user = requests.get('https://jsonplaceholder.typicode.com/users/{}'
+                        .format(argv[1])).json()
     tasks = requests.get('https://jsonplaceholder.typicode.com/users/{}/todos'
-                         .format(argv[1]))
-    done = 0
+                         .format(argv[1])).json()
+    completed = []
     total = 0
-    for task in tasks.json():
-        if task.get('completed') is True:
-            done += 1
-        total += 1
-    print("Employee {} is done with tasks({}/{}):".format(user, done, total))
-    for t in tasks.json():
+    for t in tasks:
         if t.get('completed') is True:
-            print("\t{}".format(t.get('title')))
+            completed.append(t.get('title'))
+        total += 1
+    print("Employee {} is done with tasks({}/{}):"
+          .format(user.get('name'), len(completed), total))
+    print("\n".join('\t {}'.format(i) for i in completed))
