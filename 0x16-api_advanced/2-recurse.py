ff144b81aeb375ca145a9f3668566c250533c3ef
Amy <amy.tai0120@gmail.com>
2018-08-31 03:10:49 +0000

    Q2

---
+++ 0x16-api_advanced/2-recurse.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+'''
+    module for querying Reddit API
+'''
+import requests
+
+
+def recurse(subreddit, hot_list=[], page='placeholder'):
+    '''
+        recursively query all the hottest post in a given subreddit
+    '''
+    try:
+        res = requests.get('https://www.reddit.com/r/{}/hot.json?limit=100&after={}'
+                           .format(subreddit, page),
+                           headers={"user-agent": 'ames'}).json()
+        if page is None:
+            return hot_list
+        else:
+            next_page = res['data']['after']
+            for post in res['data']['children']:
+                hot_list.append(post['data']['title'])
+            final = recurse(subreddit, hot_list, next_page)
+            return final
+    except KeyError:
+        return None
